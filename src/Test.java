32e4bccd0f5dcaa7217721cb8bb5f461406cf59d
lvfengchen <475470400@qq.com>
2020-09-25 22:38:01 +0800

    Test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,26 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        if("01".equals(DemoEnum.lfc.getCode())){
+            System.out.println("Hello lfc!");
+        }else if("02".equals(DemoEnum.dp.getCode())){
+            System.out.println("Hello dp!");
+        }else if("03".equals(DemoEnum.cbl.getCode())){
+            System.out.println("Hello cbl!");
+        }
+
+        switch(DemoEnum.getEnum("01")){
+            case lfc:
+                System.out.println("Hello lfc!");
+                break;
+            case dp:
+                System.out.println("Hello dp!");
+                break;
+            case cbl:
+                System.out.println("Hello cbl!");
+                break;
+            default:
+                System.out.println("Hello World!");
+        }
+
     }
 }
