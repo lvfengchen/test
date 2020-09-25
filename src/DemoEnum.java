441adce791f615765c453d5e8572118436b84cc4
lvfengchen <475470400@qq.com>
2020-09-25 22:11:37 +0800

    xxx

---
+++ src/DemoEnum.java
@@ -0,0 +1,32 @@
+public enum DemoEnum {
+    /**
+     * 枚举Demo
+     */
+    lfc("01","吕风琛"),
+    dp("02","段鹏"),
+    cbl("03","陈泊龙");
+    private String code;
+    private String name;
+    DemoEnum(String code ,String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static DemoEnum getEnum(String code){
+        for(DemoEnum e : values()){
+            if(code.equals(e.getCode())){
+                return e;
+            }
+        }
+        return null;
+    }
+
+}
